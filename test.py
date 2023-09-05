4c076cab909ee250f45157f276d373246f89a03f
flo-shi <kaniaflorence4@gmail.com>
2023-09-05 18:01:16 +0300

    first commit

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-import streamlit 
-print (streamlit.__version__)
+import streamlit
+import numpy as np
+import pickle
+
+# Load the saved model parameters
+with open("linear_regression_model.pkl", "rb") as f:
+    model_parameters = pickle.load(f)
+
+coefficients = model_parameters["coefficients"]
+intercept = model_parameters["intercept"]
